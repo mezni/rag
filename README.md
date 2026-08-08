f0c09d0369c45ceae50556e40712084b5fa5fa5e
M.Mezni <mamezni@gmail.com>
2026-08-08 19:55:05 -0400

    Cleanup

--- README.md
+++
@@ -1,1 +0,0 @@
-docker run -it --rm -p 8888:8888 jupyter/pyspark-notebook
--- archive/v01/generator/cdr_v01/value_objects.py
+++
@@ -1,6 +0,0 @@
-class Duration:
-    def __init__(self, seconds):
-        self.seconds = seconds
-
-    def __repr__(self):
-        return f"{self.seconds}s"
